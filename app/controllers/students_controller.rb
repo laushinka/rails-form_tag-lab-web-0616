cb655ba63e587264c7bff6688494666096f17474
laushinka <laurie.malau@gmail.com>
2016-07-04 22:14:39 -0400

    Change form tags back to old html forms

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,34 @@
+
 class StudentsController < ApplicationController
-  
+  def new
+    # @student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+    if @student.save
+    redirect_to students_path
+    else
+      render :new
+    end
+  end
+  #
+  # def create
+  # # binding.pry
+  # @student = Student.create(params.require(:student).permit(:first_name, :last_name))
+  # redirect_to students_path
+  # end
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+  #
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :other_things)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
-  get '/student/:id', to: 'students#show', as: 'student'
+  # get '/student/new', to: 'students#new', as: 'new_student'
+  resources :students #only: [:index, :create, :new]
+  get '/students/:id', to: 'students#show'
+
 end
